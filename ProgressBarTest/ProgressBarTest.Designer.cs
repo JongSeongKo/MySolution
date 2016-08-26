6b405764294131ca59f2608e937974f5aed621b0
jungjaemin <youlove0131@bistel.co.kr>
2016-08-26 17:10:57 +0900

    초기 커밋.

---
+++ ProgressBarTest/ProgressBarTest.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MyBaseCode
+{
+    partial class ProgressBarTest
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(194, 159);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(235, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Value = 50;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(368, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.Visible = false;
+            // 
+            // ProgressBarTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 404);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "ProgressBarTest";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ProgressBarTest/ProgressBarTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyBaseCode
+{
+    public partial class ProgressBarTest : Form
+    {
+        public ProgressBarTest()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+        }
+
+        
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Thread th = new Thread(InProgressBarInvoke);
+            //th.Start();
+
+            InProgressBarInvoke();
+        }
+
+        private void InProgressBarInvoke()
+        {
+            InProgressDelegate invoker = new InProgressDelegate(Progress);
+            invoker.BeginInvoke(new AsyncCallback(CallBackInvoke), invoker);
+            
+        }
+
+        private void Progress()
+        {
+            Action<int> uiInvoker = SetProgress;
+            Action<int> textInvoker = SetTextBox;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar1.Invoke(uiInvoker, i);
+                Thread.Sleep(10);
+                textBox1.Invoke(textInvoker, i);
+            }
+
+           // MessageBox.Show("끝");
+        }
+
+        private void SetTextBox(int progress)
+        {
+            textBox1.Text = progress.ToString();
+        }
+
+        private void SetProgress(int progress)
+        {
+            progressBar1.Value = progress;
+        }
+
+        void CallBackInvoke(IAsyncResult iar)
+        {
+            InProgressDelegate result =  iar.AsyncState as InProgressDelegate;
+
+            result.EndInvoke(iar);
+
+        }
+
+
+        private delegate void InProgressDelegate();
+
+
+
+    }
+}
